db08c51d41fd91c52db6418eb0d504e414128690
Henrik <henrik.gjoertz@fu-berlin.de>
2017-12-11 17:49:00 +0100

    added setup files

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+import sys
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ['urbanbroccoli']
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+description = '''
+Simulator of system of charged particles...
+'''
+
+setup(
+    cmdclass={'test': PyTest},
+    name='cuddlyguacamole',
+    version='0.1.0',
+    author='Henrik Gjoertz',
+    author_email='henrik.gjoertz@fu-berlin.de',
+    url='https://github.com/slemrik/cuddly-guacamole',
+    description=description,
+    packages=['cuddlyguacamole', 'cuddlyguacamole.test'],
+    setup_requires=['pytest-runner',],
+    install_requires=['numpy'],
+    tests_require=['pytest'],
+    zip_safe=False)
