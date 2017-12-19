c914636ec6fbfa723c5d837d74a478fd7c1674e1
Henrik <henrik.gjoertz@fu-berlin.de>
2017-12-19 11:18:29 +0100

    wrote more lj code... still still not finished

--- cuddlyguacamole/neighbourlist.py
+++ cuddlyguacamole/neighbourlist.py
@@ -14,20 +14,20 @@ def verlet_neighbourlist(box, r_cut, r_skin):
 	"""Verlet neighbourlist computation: for each of the particles in the array *particles*,
 	we compute a list of particles that lie within a cutoff radius r_cut+r_skin. Returns 
 	the array particles with an updated neighbourlist for each particle.
-    
-    arguments:
-    	box (Box object): includes array *box.particles* (array-like of Particle) listing all the
-			Particle objects in the system
-    	r_cut: verlet cutoff radius
-    	r_skin: size of verlet skin region
-    """
+
+	arguments:
+		box (Box object): includes array *box.particles* (array-like of Particle) listing all the
+		Particle objects in the system
+		r_cut: verlet cutoff radius
+		r_skin: size of verlet skin region
+	"""
 
 	for i, particlei in enumerate(box.particles):
 		particlei.neighbourlist = [] # clear neighbourlist for each particle		
-		for particlej in box.particles[i+1:]:
-			if np.linalg.norm(enforce_pbc(particlei.position - particlej.position,box.size)) < r_cut + r_skin
-				particlei.neighbourlist.append(particlej) #this updates box.particles[i].neighbourlist as well?
-				particlej.neighbourlist.append(particlei)
+	for particlej in box.particles[i+1:]:
+		if np.linalg.norm(enforce_pbc(particlei.position - particlej.position,box.size)) < r_cut + r_skin
+			particlei.neighbourlist.append(particlej) #this updates box.particles[i].neighbourlist as well?
+			particlej.neighbourlist.append(particlei)
 
 	return box
 
