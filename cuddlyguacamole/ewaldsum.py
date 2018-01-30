fc3199fb4fdc84afdb5a9184d3aa8b3c8d0c8469
JiashengLai <33054127+JiashengLai@users.noreply.github.com>
2018-01-30 08:24:14 +0800

    Update ewaldsum.py

--- cuddlyguacamole/ewaldsum.py
+++ cuddlyguacamole/ewaldsum.py
@@ -8,9 +8,6 @@ from scipy.special import erfc
 from scipy import exp, pi
 
 
-
-
-
 def energy(position,  q, cell, .......):
     """
     Arguments:
@@ -26,49 +23,57 @@ def energy(position,  q, cell, .......):
 
 
 
+
 def self_potential ##
 
+def 
 
 #energy calculation formula. with reference to equation 39 in page 7 of the pdf(link found in first line of this file). 
 def short_energy_sum (i, r, q, cell, alpha, cutoff_rspace):
     
     for j in range(0, len(q)):
-        rij = r[i, :] - r[j, :]
+        
    
         
         
     return Vr
 
 
-def long_e (i, r, q, cell, alpha, cutoff_kspace, area):
+def total_long_energy (i, r, q, cell, alpha, cutoff_kspace):
     long_e = 0
     
     for j in range(0, len(q)):
         rij = r[i, :] - r[j, :]
-       
-   
+
             
     long_pre = 1/(2*V*epsilon_0)     #prefactor of the long-ranged term         
-    k                                #???reciprocal vector need to be defined 
+    k                                #????reciprocal vector need to be defined 
     k2 = k**2 
     sigma2 = sigma** 2 
-    s_k = sum( q*np.exp(k*r)         #structure factor 
-    s_k2= s_k**2                     #sqaure of structure factor    
+    s_k = sum( q*np.exp(k*r))         #structure factor 
+    s_k2= s_k**2                      #sqaure of structure factor    
 
     midpart = np.sum(exp(-sigma2*k2/2)/k2) 
      
-        long_energy_sum = long_pre*midpart*s_k2
-    return long_energy_sum
+        total_long_energy = long_pre*midpart*s_k2
+    return total_long_energy
 
 
-def total_self_energy(i, r, q, cell, alpha):                 #general algo. done 
-    self_energy = 0 
-    self_potential_sum = 0 
-    self_pre = 1/(4*pi*eplison_0*sigma*np.sqrt(2*np.pi))     #prefactor of the self-term
+def total_s_energy(self):                                 #general algo. done 
+    s_energy = 0 
+    s_total_potential = 0 
+    s_pre = 1/(4*pi*eplison_0*sigma*np.sqrt(2*np.pi))     #prefactor of the self-term
               
-    for i in range(0, len(q)):                               # !! particle code to be corrected to system.  
-        self_potential_sum += self_potential(i)              # !! def self_potential 
+    for i in range(0, len(q)):                            # ==1  particle code to be corrected to system.  
+        total_s_potential += s_potential(i)               # !! def self_potential 
       
-    total_self_energy = self_pre*self_potential_sum 
+    total_s_energy = s_pre*s_total_potential
          
-    return total_self_energy
+    return total_s_energy
+
+
+def kspace (self) :
+    s_k=0 
+    for i in range ():
+        s_k += charge(i)*exp(k*position(i))
+    return s_k 
