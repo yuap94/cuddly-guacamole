0cf9ff278ffd6c8333bd99c218b0f7ed57d5729e
JiashengLai <33054127+JiashengLai@users.noreply.github.com>
2018-02-21 22:35:50 +0800

    Update ewaldsum.py

--- cuddlyguacamole/ewaldsum.py
+++ cuddlyguacamole/ewaldsum.py
@@ -1,43 +1,7 @@
-#Mathematical derivation. with reference to  http://micro.stanford.edu/mediawiki/images/4/46/Ewald_notes.pdf 
-
-import numpy as np
-import neighbourlist
-import system
-from numpy.linalg import norm
-from scipy.special import erfc
-from scipy import exp, pi
-
-
-def energy(position,  q, cell, .......):
-
-    Energy_short  = short_energy(i, r, q, cell, cutoff_rspace)m
-    Energy_long   = long_energy(i, r, q, cutoff_kspace)
-    Energy_self   = self_energy(i, r, q, cell)
-    
-    return Energy_short+Energy_long-Engery_self
-
-
-
-
-def self_potential ##
-
-#energy calculation formula. with reference to equation 39 in page 7 of the pdf(link found in first line of this file). 
-def short_energy_sum (i, r, q, cell, alpha, cutoff_rspace):
-    
-    for j in range(0, len(q)):
-        
-   
-        
-        
-    return Vr
-
-
-
 
 
 
 def k_energy (i, r, q, cutoff_kspace):
-    
     
     
     for k_i in range (-k_c,k_c+1)
@@ -50,7 +14,7 @@ def k_energy (i, r, q, cutoff_kspace):
     #prefactor, V value ???
     pre_fac = 1/(2*V*epsilon_0)  
 
-    #Structure factor
+    #Structure factor 
     
     s_k = sum( q*np.exp(k*r))          
     s_k2= s_k**2              
@@ -74,38 +38,13 @@ def k_cut_off (self) :
                       
  
 def str_fac (self) :
-    s_k=0 
+    
     for i in range ():
-        s_k += charge(i)*exp(k*position(i))
+        s_k=0 
+        
+        for j in range (p_num) :
+            s_k += charge(j)*np.cos(np.dot(k,position(j)))
+            
     return s_k 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def total_s_energy(self):                                 #general algo. done 
-    s_energy = 0 
-    s_total_potential = 0 
-    s_pre = 1/(4*pi*epsilon_0*sigma*np.sqrt(2*np.pi))     #prefactor of the self-term
-              
-    for i in range(0, len(q)):                            # ==1  particle code to be corrected to system.  
-        total_s_potential += s_potential(i)               # !! def self_potential 
-      
-    total_s_energy = s_pre*s_total_potential
-         
-    return total_s_energy
 
 
