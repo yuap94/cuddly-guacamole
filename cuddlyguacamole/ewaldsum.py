8bf33e794a888e0bee871d1a5b4a4f92329ad464
JiashengLai <33054127+JiashengLai@users.noreply.github.com>
2018-02-21 23:23:01 +0800

    Update ewaldsum.py

--- cuddlyguacamole/ewaldsum.py
+++ cuddlyguacamole/ewaldsum.py
@@ -1,42 +1,41 @@
 
 
 
-def k_energy (i, r, q, cutoff_kspace):
+def k_energy (i, r, q, k_c):
+    
     
     
     for k_i in range (-k_c,k_c+1)
         for k_j in range (-k_c,k_c+1)
             for k_k in range (-k_c,k_c+1) 
+            
                 #Reciprocal vector 
                 k = 2.0*np.pi*np.array([k_i / (box[0]), k_j / (box[1]), k_k / (box[2])] )                               
-                k2 = k*k  
+                k_sq = k*k  
     
-    #prefactor, V value ???
-    pre_fac = 1/(2*V*epsilon_0)  
+                #prefactor, V value ???
+                pre_fac = 1/(2*V*epsilon_0)             
 
-    #Structure factor 
-    
-    s_k = sum( q*np.exp(k*r))          
-    s_k2= s_k**2              
-
-    exp_term = np.sum(exp(-sigma2*k2/2)/k2) 
+                exp_term += np.exp(-(sigma**2)*k_sq/2)/k_sq) 
      
-         k_space_energy= pre_fac*s_k2*exp_term
+         k_space_energy= pre_fac*(s_k**2)*exp_term
         
     return k_space_energy
 
 
 def k_cut_off (self) :
-    #accuracy set to 1e-6 gives p 
-    p = ????
-    k_c = 2*p/r_cut 
-    #Sigma
-    sigma = r_c / np.sqrt (2*p) 
-    sigma2 = sigma*sigma 
+    #!!! accuracy set to 1e-6 gives p, make p global 
+    k_c = (2*p)/r_cut 
     
+    return k_c 
 
-                      
+def sigma (self) : 
  
+    sigma = r_c / np.sqrt (2*p) 
+    
+    return sigma 
+    
+                     
 def str_fac (self) :
     
     for i in range ():
